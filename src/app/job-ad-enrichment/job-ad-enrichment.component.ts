31df81b11612a791301993b87141fad4f08b471d
Patrik Olsson <patrik.olsson@arbetsformedlingen.se>
2023-04-14 14:56:47 +0200

    Added empty button

--- src/app/job-ad-enrichment/job-ad-enrichment.component.ts
+++ src/app/job-ad-enrichment/job-ad-enrichment.component.ts
@@ -37,4 +37,9 @@ export class JobAdEnrichmentComponent {
       this.enrichments = value
     })
   }
+
+  empty() {
+    this.headlineInputControl.setValue("")
+    this.textInputControl.setValue("")
+  }
 }
